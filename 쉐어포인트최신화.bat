7d2c9ed584d314d20c5465a1f4953fb2ae993818
성민석 <70127676+SungMinseok@users.noreply.github.com>
2024-02-07 10:39:00 +0900

    240207

--- 쉐어포인트최신화.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set "source=C:\Users\mssung\OneDrive\파이썬프로젝트\CLM(CheckListMaker)"
-set "destination=C:\Users\mssung\OneDrive - Webzen Inc\R2M도구\CLM(CheckListMaker)"
-
-xcopy "%source%" "%destination%" /E /EXCLUDE:%source%\exclude.txt
